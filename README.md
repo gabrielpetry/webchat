b39c8e2548b42b83c079efaed479b0382e25e3d2
Gabriel Petry <gabrielpetry@outlook.com>
2018-11-16 14:32:16 -0200

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Webchat
+Esse é um webchat simples, foi criado utilizando a [documentação do SocketIO](https://socket.io/get-started/chat)
+
+# Como usar
+1. Faça o download do repositório
+    - git clone git@github.com:gabrielpetry/webchat.git
+2. Instale as depêndencias
+    - npm install
+3. Inicia o servidor
+    - npm start
+
+Esse é um wrapper da [documentação do SocketIO](https://socket.io/get-started/chat) mostrando como criar um webchat.
+
+E pode ser utilizado para extender as funcionabilidades e aplicabilidade para qualquer coisa :) 
+
---
+++ server.js
@@ -0,0 +1,22 @@
+// Realiza o require do express, http, e socketio
+var app = require('express')();
+// passa o express para o http-server
+var http = require('http').Server(app);
+// passa o http-server par ao socketio
+var io = require('socket.io')(http);
+
+// cria uma rota para fornecer o arquivo index.html
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+// sempre que o socketio receber uma conexão vai devoltar realizar o broadcast dela
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+// inicia o servidor na porta informada
+http.listen(3000, function(){
+  console.log('Servidor rodando em: http://localhost:3000');
+});
